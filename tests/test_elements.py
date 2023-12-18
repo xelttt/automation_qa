528ba807678dbc96ee0bd5e60eba781f23a0a921
xelt <xelttt@icloud.com>
2023-12-18 21:35:25 +0300

    created test_elements.py / TextBox added elements_page.py, elements_page_locators.py, test_elements.py, generator.py, data.py

---
+++ tests/test_elements.py
@@ -0,0 +1,13 @@
+import time
+from conftest import driver
+from pages.elements_page import TextBoxPage
+
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            input_data = text_box_page.fill_all_fields()
+            output_data = text_box_page.check_filled_form()
+            assert input_data == output_data
