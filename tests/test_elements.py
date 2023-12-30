f7fe31cf2fe88e38f551b21902d0ac7febd8efd4
xelt <xelttt@icloud.com>
2023-12-30 21:14:42 +0300

    fixed go_to_element in BasePage update tests elements added test checkboxes

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,20 +1,31 @@
 import time
 from conftest import driver
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
+
 
 class TestElements:
     class TestTextBox:
-
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_cur_adr, output_per_adr = text_box_page.check_filled_form()
-            assert full_name == output_name
-            assert email == output_email
-            assert current_address == output_cur_adr
-            assert permanent_address == output_per_adr
+            assert full_name == output_name, "the full name does not match"
+            assert email == output_email, "the email does not match"
+            assert current_address == output_cur_adr, "the current address does not match"
+            assert permanent_address == output_per_adr, "the permanent address does not match"
 
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            print(input_checkbox)
+            print(output_result)
+            assert input_checkbox == output_result, "checkboxes have not been selected"
 
 
 
