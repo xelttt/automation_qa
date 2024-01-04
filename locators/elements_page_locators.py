cec1141053459627ba6514e682d648ac6e02003e
xelt <xelttt@icloud.com>
2024-01-04 21:41:32 +0300

    update tests elements
    added class:
     TestButtonPage
    added tests:
    - test_different_click_on_the_buttons

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -48,3 +48,13 @@ class WebTablePageLocators:
 
     # delete
     NO_ROWS_FOUND = (By.CSS_SELECTOR, 'div[class="rt-noData"]')
+
+class ButtonsPageLocators:
+    DOUBLE_BUTTON = (By.CSS_SELECTOR, 'button[id="doubleClickBtn"]')
+    RIGHT_CLICK_BUTTON = (By.CSS_SELECTOR, 'button[id="rightClickBtn"]')
+    CLICK_ME_BUTTON = (By.XPATH, '//div[3]/button')
+
+    # result
+    SUCCESS_DOUBLE = (By.CSS_SELECTOR, 'p[id="doubleClickMessage"]')
+    SUCCESS_RIGHT = (By.CSS_SELECTOR, 'p[id="rightClickMessage"]')
+    SUCCESS_CLICK_ME = (By.CSS_SELECTOR, 'p[id="dynamicClickMessage"]')
--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,7 +1,7 @@
 import random
 import time
 from conftest import driver
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage
 
 
 class TestElements:
@@ -84,3 +84,14 @@ class TestElements:
             count = web_table_page.select_up_to_some_rows()
             assert count == [5, 10, 20, 25, 50, 100], 'The number of rows in the table has not been changed or has changed incorrectly'
 
+    class TestButtonsPage:
+
+        def test_different_click_on_the_buttons(self, driver):
+            buttons_page = ButtonsPage(driver, 'https://demoqa.com/buttons')
+            buttons_page.open()
+            double = buttons_page.click_on_different_button('double')
+            right = buttons_page.click_on_different_button('right')
+            click = buttons_page.click_on_different_button('click')
+            assert double == "You have done a double click", 'The double click button was not pressed'
+            assert right == "You have done a right click", 'The right click button was not pressed'
+            assert click == "You have done a dynamic click", 'The dynamic click button was not pressed'
