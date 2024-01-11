55354a70f648c8aa45673f4af885414a9c643bed
xelt <xelttt@icloud.com>
2024-01-11 18:24:37 +0300

    added TestTabs

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -3,7 +3,7 @@ import time
 import datetime
 
 from conftest import driver
-from pages.widgets_page import AccordianPage, AutoCompletePage, DatePickerPage, SliderPage, ProgressBarPage
+from pages.widgets_page import AccordianPage, AutoCompletePage, DatePickerPage, SliderPage, ProgressBarPage, TabsPage
 
 
 class TestWidgets:
@@ -72,3 +72,17 @@ class TestWidgets:
             progress_bar.open()
             before, after = progress_bar.change_progress_bar_value()
             assert before != after, 'the progress bar value has not been changed'
+
+    class TestTabsPage:
+
+        def test_tabs(self, driver):
+            tabs_page = TabsPage(driver, 'https://demoqa.com/tabs')
+            tabs_page.open()
+            what_button, what_content = tabs_page.check_tabs('what')
+            origin_button, origin_content = tabs_page.check_tabs('origin')
+            use_button, use_content = tabs_page.check_tabs('use')
+            more_button, more_content = tabs_page.check_tabs('more')
+            assert what_button == "What" and what_content != 0, 'the tab "what" was not pressed or the text is missing'
+            assert origin_button == "Origin" and origin_content != 0, 'the tab "Origin" was not pressed or the text is missing'
+            assert use_button == "Use" and use_content != 0, 'the tab "Use" was not pressed or the text is missing'
+            assert more_button == "More" and more_content != 0, 'the tab "More" was not pressed or the text is missing'
