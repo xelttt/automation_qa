970f21e0c447d7c1a47414b3ac131d35421bcf74
xelt <xelttt@icloud.com>
2024-01-13 19:45:25 +0300

    added test_selectable

--- tests/test_interactions.py
+++ tests/test_interactions.py
@@ -1,14 +1,24 @@
-from pages.interactions_page import SortablePage
+from pages.interactions_page import SortablePage, SelectablePage
 from conftest import driver
 
 class TestInteractions:
 
-     class TestSortablePage:
+    class TestSortablePage:
 
-         def test_sortable(self, driver):
+        def test_sortable(self, driver):
              sortable_page = SortablePage(driver, 'https://demoqa.com/sortable')
              sortable_page.open()
              list_before, list_after = sortable_page.change_list_order()
              grid_before, grid_after = sortable_page.change_grid_order()
              assert list_before != list_after, 'the order of the list has not been changed'
              assert grid_before != grid_after, 'the order of the grid has not been changed'
+
+    class TestSelectablePage:
+
+        def test_selectable(self, driver):
+            selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+            selectable_page.open()
+            item_list = selectable_page.select_list_item()
+            item_grid = selectable_page.select_grid_item()
+            assert len(item_list) > 0, 'no elements were selected'
+            assert len(item_grid) > 0, 'no elements were selected'
